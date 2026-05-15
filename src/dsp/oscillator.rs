33d0cd1580087ad4ae3a5ee6150f1d69d8395481
xiwire <personal@xiwi.re>
2026-05-15 17:42:15 +0200

    make public

--- src/dsp/oscillator.rs
+++ src/dsp/oscillator.rs
@@ -6,11 +6,11 @@ pub struct Sine {
 }
 
 impl Sine {
-    fn new(freq: f32) -> Self {
+    pub fn new(freq: f32) -> Self {
         Sine{ pos: 0.0, freq: freq }
     }
 
-    fn process(&mut self, sample_rate: usize) -> f32 {
+    pub fn process(&mut self, sample_rate: usize) -> f32 {
         self.pos += self.freq / (sample_rate as f32);
         self.pos %= 1.0;
 
