4b4446ff49bd5bba6b86347023556bdef90d74a4
xiwire <personal@xiwi.re>
2026-05-16 13:22:54 +0200

    rewrite wav reader to use new buffers

--- src/files/audio.rs
+++ src/files/audio.rs
@@ -1,13 +1,23 @@
 use std::path::Path;
 
-use crate::dsp::audio_buffer::AudioBuffer;
+use wave_stream::open_wav::OpenWav;
 
-#[cfg(feature = "desktop")]
-pub fn from_path(filepath: &str) -> AudioBuffer::<f32> {
+use crate::dsp::audio_buffer::DynamicMonoBuffer;
+
+pub fn from_path(filepath: &str) -> DynamicMonoBuffer<f32> {
     use wave_stream::{read_wav_from_file_path, wave_reader::StreamOpenWavReader};
 
     let wav_reader = read_wav_from_file_path(Path::new(filepath)).unwrap();
-    let stream = wav_reader.get_stream_f32_reader().unwrap().into_iter().map(|i| i.unwrap().front_left.unwrap());
+    let size = wav_reader.len_samples();
+    let stream = wav_reader
+        .get_stream_f32_reader()
+        .unwrap()
+        .into_iter()
+        .map(|i| i.unwrap().front_left.unwrap());
 
-    AudioBuffer::<f32>::from_iter(stream)
+    let mut buffer = DynamicMonoBuffer::<f32>::new(size);
+
+    buffer.write_from_iter(stream);
+
+    buffer
 }
--- src/files/mod.rs
+++ src/files/mod.rs
@@ -1,3 +1,6 @@
+//! For desktop files.
+#[cfg(feature = "desktop")]
+
 /// For managing audio files (.WAV, .MP3, .AIFF) and the such and using them as
 /// [`crate::dsp::audio_buffer::AudioBuffer`].
 pub mod audio;
