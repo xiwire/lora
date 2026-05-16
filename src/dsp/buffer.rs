280ac8cc162f1e13aa3f1ca98558bfd6bd177cbe
xiwire <personal@xiwi.re>
2026-05-16 19:45:02 +0200

    rewrite buffer to use ringbuf

---
+++ src/dsp/buffer.rs
@@ -0,0 +1,57 @@
+use ringbuf::{HeapRb, StaticRb, traits::{Consumer, Producer}};
+use core::default::Default;
+
+pub trait AudioBuffer {
+    type T;
+    fn write(&mut self, data: &[Self::T]) -> usize;
+    fn read(&mut self, output: &mut [Self::T]) -> usize;
+    fn clear(&mut self) -> usize;
+}
+
+/// Fixed size ring buffer
+pub struct FixedMonoBuffer<T, const SIZE: usize> 
+where 
+    T: Default + Copy
+{
+    pub contents: StaticRb<T, SIZE>,
+}
+
+pub struct DynamicMonoBuffer<T> 
+where 
+    T: Default + Copy
+{
+    pub contents: HeapRb<T>,
+}
+
+impl<T> DynamicMonoBuffer<T>
+where 
+    T: Default + Copy
+{
+   pub fn new(capacity: usize) -> Self {
+      Self { contents: HeapRb::<T>::new(capacity) } 
+   } 
+}
+
+impl<T> AudioBuffer for DynamicMonoBuffer<T>
+where 
+    T: Default + Copy
+{
+    type T = T;
+    fn write(&mut self, data: &[Self::T]) -> usize {
+        self.contents.push_slice(data)
+    }
+
+    fn read(&mut self, output: &mut [Self::T]) -> usize {
+        self.contents.pop_slice(output)
+    }
+
+    fn clear(&mut self) -> usize {
+        self.contents.clear()
+    }
+}
+
+/// Used for when audio is interleaved stereo.
+pub struct InterleavedStereoBuffer {}
+
+/// Used when audio is meant to be used in FFT/convolution algorithms.
+pub struct PlanarStereoBuffer {}
--- src/files/audio.rs
+++ src/files/audio.rs
@@ -1,23 +1,27 @@
 use std::path::Path;
 
-use wave_stream::open_wav::OpenWav;
+use wave_stream::{open_wav::OpenWav, wave_reader::RandomAccessOpenWavReader};
 
-use crate::dsp::audio_buffer::DynamicMonoBuffer;
+use crate::dsp::buffer::{AudioBuffer, DynamicMonoBuffer};
 
 pub fn from_path(filepath: &str) -> DynamicMonoBuffer<f32> {
+    // TODO: should actually accept multiple audio formats
     use wave_stream::{read_wav_from_file_path, wave_reader::StreamOpenWavReader};
 
     let wav_reader = read_wav_from_file_path(Path::new(filepath)).unwrap();
     let size = wav_reader.len_samples();
-    let stream = wav_reader
+
+    let mut buffer = DynamicMonoBuffer::<f32>::new(size);
+
+    let stream  = wav_reader
         .get_stream_f32_reader()
         .unwrap()
         .into_iter()
         .map(|i| i.unwrap().front_left.unwrap());
 
-    let mut buffer = DynamicMonoBuffer::<f32>::new(size);
+    let vector = Vec::<f32>::from_iter(stream);
 
-    buffer.write_from_iter(stream);
+    buffer.write(vector.as_slice());
 
     buffer
 }
